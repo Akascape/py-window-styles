a1f819f4d653c1aa6bd01434551acfd6031e7012
Akash Bora <89206401+Akascape@users.noreply.github.com>
2023-06-24 12:32:37 +0530

    Add files via upload

---
+++ pywinstyles/__init__.py
@@ -0,0 +1,15 @@
+"""
+Custom window styles for window 11
+Author: Akash Bora
+License: MIT
+Homepage: https://github.com/Akascape/py-window-styles
+"""
+
+__version__ = '1.0'
+
+from .py_win_style import apply_style
+from .py_win_style import change_header_color
+from .py_win_style import change_border_color
+from .py_win_style import change_title_color
+from .py_win_style import ChangeDWMAttrib
+from .py_win_style import ChangeDWMAccent
